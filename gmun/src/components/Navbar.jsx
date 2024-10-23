1680998da6c5b06418f8340d67b4645d4aeaecbd
aditigarg17 <aditigarg484@gmail.com>
2024-10-23 18:28:35 +0530

    committee page structure added

--- gmun/src/components/Navbar.jsx
+++ gmun/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link> {/* Link to the landing page */}
+        </li>
+        <li>
+          <Link to="/committe">Committee</Link> {/* Link to the Committee page */}
+        </li>
+        {/* You can add more links here if needed */}
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;

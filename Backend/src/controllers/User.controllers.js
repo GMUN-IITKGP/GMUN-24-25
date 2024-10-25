b148f929598140c4fb73d7081353db71b1814bf0
Harshit Soni <kratin67soni@gmail.com>
2024-10-25 18:01:04 +0530

    added Backend to the Project

---
+++ Backend/src/controllers/User.controllers.js
@@ -0,0 +1,142 @@
+import { User } from "../models/User.model.js";
+import { asyncHandler } from "../utils/asynchandler.js";
+
+const generateAccessandRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+    if (!fullName) {
+      throw new Error("Full name is required");
+    }
+
+    if (!email) {
+      throw new Error("Email is required");
+    }
+
+    if (!password) {
+      throw new Error("Password is required");
+    }
+
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+      throw new Error(400, "User already exists with this username or email");
+    }
+
+    const user = await User.create({ fullName, email, password });
+
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    //checking if User is Created or Not
+    if (!createdUser) {
+      throw new Error(
+        500,
+        "Something went wrong while registering the User"
+      );
+    }
+
+    res.status(201).json(createdUser);
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+    if (!email) {
+      throw new Error("Email is required");
+    }
+
+    if (!password) {
+      throw new Error("Password is required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const isMatch = await user.checkPassword(password);
+
+    if (!isMatch) {
+      throw new Error("Invalid credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessandRefreshToken(
+      user._id
+    );
+
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!loggedInUser) {
+      res.status(500);
+      throw new Error("Couldn't login user");
+    }
+
+    const options = {
+      httpOnly: false,
+      secure: true,
+    };
+
+    res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json({
+        loggedInUser,
+        accessToken,
+        refreshToken,
+      });
+});
+
+const logoutUser = asyncHandler(async (req, res, next) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: false,
+    secure: true,
+    samesite: "none",
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json({
+      "User Logged Out": "Successfully",
+    });
+});
+
+export { registerUser, loginUser, logoutUser };
---
+++ Backend/src/models/User.model.js
@@ -0,0 +1,69 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 8);
+
+  next();
+});
+
+UserSchema.methods.checkPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+//to generate refreshToken
+UserSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", UserSchema);

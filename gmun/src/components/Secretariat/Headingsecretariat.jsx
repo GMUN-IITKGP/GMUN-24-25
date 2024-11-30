2f2290820f2e990937ae2acec9aa30661df7bf4c
Workspace-Member <heyykayos@gmail.com>
2024-11-30 23:20:50 +0530

    Secretariat page

---
+++ gmun/src/components/Secretariat/Headingsecretariat.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Secretariat.css";
+
+
+function Heading1() {
+    return(
+        <>
+            <h1 className="heading-secretariat">
+                SECRETARIAT
+            </h1>
+        </>
+    )
+}
+
+function Heading2() {
+    return(
+        <>
+            <h1 className="heading-secretariat">
+                TEAM
+            </h1>
+        </>
+    )
+}
+
+export {Heading1, Heading2} ;

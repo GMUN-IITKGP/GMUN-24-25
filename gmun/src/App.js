4ebe7890f734f150e32e57d80fda87c71939ccef
aditigarg17 <aditigarg484@gmail.com>
2024-12-01 00:41:17 +0530

    pushed changes in committee page

--- gmun/src/App.js
+++ gmun/src/App.js
@@ -1,37 +1,18 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar";
-import ContactForm from "./components/Contacts";
-import GuidePage from "./components/Guide";
-import { useState } from "react";
-import EditProfile from "./components/EditDetails";
-import Committe from "./components/Committe";
-import Gallery from "./components/Gallery";
-import Committee from "./components/Committee";
+
 const App = () => {
-  // const [isUser, setIsUser] = useState(false);
   return (
-    <>
-      <BrowserRouter>
-        <NavBar />
-
-        <Routes>
-
-          <Route path="/" element={<Landing />} />
-          <Route path="/navbar" element={<NavBar />} />
-
-          <Route path="/profile" element={<Profile />} />
-
-          <Route path="/contact" element={<ContactForm />} />
-          <Route path="/guide" element={<GuidePage />} />
-
-          <Route path="/EditDetails" element={<EditProfile />} />
-          <Route path="/Committe" element={<Committe />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/committees" element={<Committee />} />
-        </Routes>
-
-      </BrowserRouter>
-    </>
+    <div>
+      {/* NavBar will always be displayed */}
+      <NavBar />
+      
+      {/* Render the child route components using Outlet */}
+      <div>
+        <Outlet />
+      </div>
+    </div>
   );
 };
 

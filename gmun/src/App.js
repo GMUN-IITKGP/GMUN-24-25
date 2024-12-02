e58de7f59a433dcb683e90d3e809d3aaf9c04d21
sagnikde16 <sagnikde191@gmail.com>
2024-12-02 15:52:25 +0530

    Chatbox Frontend

--- gmun/src/App.js
+++ gmun/src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import { io } from "socket.io-client";
+import Chatbox from "./Chatbox";
+import ChatIcon from "./ChatIcon";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
-
 const App = () => {
+  const [isChatOpen, setIsChatOpen] = useState(false); // Toggle Chatbox visibility
+  const socket = useMemo(() => io("http://localhost:3000", { withCredentials: true }), []);
+
+  useEffect(() => {
+    // Clean up the socket connection on unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  const toggleChat = () => {
+    setIsChatOpen(!isChatOpen);
+  };
   return (
     <div>
       {/* Navbar will always be displayed */}
@@ -14,6 +28,10 @@ const App = () => {
       {/* Render the child route components using Outlet */}
       <div>
         <Outlet />
+      </div>
+      <div className="App">
+        {isChatOpen && <Chatbox socket={socket} />}
+        <ChatIcon onClick={toggleChat} />
       </div>
 
     </div>

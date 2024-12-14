a41254767def1d9bca28f57a1db06fc4c30636d8
rajShaibya <rajshaibya@gmail.com>
2024-12-14 20:39:35 +0530

    fixed overlapping styles

--- gmun/src/pages/aboutUs.jsx
+++ gmun/src/pages/aboutUs.jsx
@@ -4,15 +4,15 @@ import '../styles/about.css';
 const aboutUs = () => {
   return (
     <div>
-      <div className="contain">
+      <div className="contain1">
       <div className="box">
       <div className="head">Communiqué</div>
-      <div className="content">Recognizing the importance of communication and soft skills in personal and professional life and keeping in mind the diverse background of the students, Communiqué, in a greater picture, strives towards achieving its goal to inspire, blossom confidence, and equip each KGPian with career building soft skills.</div>
+      <div className="content1">Recognizing the importance of communication and soft skills in personal and professional life and keeping in mind the diverse background of the students, Communiqué, in a greater picture, strives towards achieving its goal to inspire, blossom confidence, and equip each KGPian with career building soft skills.</div>
       </div>
 
       <div className="box">
         <div className="head">Global Model United Nations</div>
-        <div className="content">Model United Nations is an academic simulation of the United Nations where students play the role of delegates from different countries and attempt to solve real world issues with policies and perspectives of an assigned country. These are exercises beneficial for those interested in learning more about the UN. They engage in negotiations, discussions and lively debate over various agendas to come to a consensus with the other nations.</div>
+        <div className="content1">Model United Nations is an academic simulation of the United Nations where students play the role of delegates from different countries and attempt to solve real world issues with policies and perspectives of an assigned country. These are exercises beneficial for those interested in learning more about the UN. They engage in negotiations, discussions and lively debate over various agendas to come to a consensus with the other nations.</div>
       </div>
       </div>
     </div>

20f0d7c994c4381da3a472dfa34981d3812bd4eb
AaryanBist <mail2aaryanbist@gmail.com>
2024-12-30 14:01:47 +0530

    updated committees in carousel

--- gmun/src/components/carousel.jsx
+++ gmun/src/components/carousel.jsx
@@ -6,19 +6,19 @@ const Carousel = () => {
         <div id="wrapper">
             <div id="image">
                 <div className="image i1">
-                    <img src="https://i.imgur.com/jKeLfYT.png" alt="" />
+                    <img src="https://i.imgur.com/jALdWIY.png" alt="" />
                 </div>
                 <div className="image i2">
-                    <img src="https://i.imgur.com/VgDkCcJ.png" alt="" />
+                    <img src="https://i.imgur.com/erXe8cQ.png" alt="" />
                 </div>
                 <div className="image i3">
                     <img src="https://i.imgur.com/syRkxXK.png" alt="" />
                 </div>
                 <div className="image i4">
-                    <img src="https://i.imgur.com/ps9xN6W.png" alt="" />
+                    <img src="https://i.imgur.com/tapTvcA.png" alt="" />
                 </div>
                 <div className="image i5">
-                    <img src="https://i.imgur.com/bWFOtDg.png" alt="" />
+                    <img src="https://i.imgur.com/jKeLfYT.png" alt="" />
                 </div>
             </div>
         </div>
